b11c110202e9071d6dbeff5dca3679dfeec5e102
Zoe <zoe.mbikayi@gmail.com>
2025-06-17 20:09:48 +0200

    database session moved to async

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,20 +1,30 @@
-from sqlmodel import Session, SQLModel, create_engine
-from app.core.config import settings
-from typing import Generator
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from typing import AsyncGenerator
 
-# Create the engine
-engine = create_engine(
+from app.core.config import settings
+
+# The engine
+engine = create_async_engine(
     str(settings.DATABASE_URL),
     echo=settings.DEBUG,
     pool_pre_ping=True,
 )
 
+# The factory
+async_session = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
 # Session dependency
-def get_session() -> Generator[Session, None, None]:
-    with Session(engine) as session:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
         yield session
 
-# For testing overrides / CLI tools
+# For testing , Alembic and all that...
 def get_engine():
     return engine
 
